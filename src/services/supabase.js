22721a6492a587a0d7d01ed5e7d7c2df8132c050
Raman Andreyuk <romaandreyuk@gmail.com>
2024-04-27 20:14:33 +0200

    Uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://rznhfhojldmnudrgtowi.supabase.co";
+export const supabaseUrl = "https://rznhfhojldmnudrgtowi.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6bmhmaG9qbGRtbnVkcmd0b3dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzODg0MjMsImV4cCI6MjAyODk2NDQyM30.Wa_lItErpY__Iw0c-Q1ztTB5H8JyfhwY57oEnSpx54Q";
 

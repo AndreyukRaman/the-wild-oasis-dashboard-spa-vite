9e836afbdb7b9663d3f9d8345c57fc7a034f324f
Raman Andreyuk <romaandreyuk@gmail.com>
2025-08-31 00:43:13 +0200

    changed public supabase key for a new project

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://rznhfhojldmnudrgtowi.supabase.co";
+export const supabaseUrl = "https://rbupsxdvjbadgeziqpon.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6bmhmaG9qbGRtbnVkcmd0b3dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzODg0MjMsImV4cCI6MjAyODk2NDQyM30.Wa_lItErpY__Iw0c-Q1ztTB5H8JyfhwY57oEnSpx54Q";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJidXBzeGR2amJhZGdlemlxcG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5ODQxNzcsImV4cCI6MjA3MTU2MDE3N30.8gSsOVN3dcnAC8h2tkD0Z8mpvvL3nJFpoJhVXQcOpyw";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
